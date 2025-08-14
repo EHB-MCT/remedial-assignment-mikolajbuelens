5f1d00f7c5d3fbf06ded16a184a101da1b159a56
MikolajBuelens <mikolaj.buelens@student.ehb.be>
2025-08-14 13:32:59 +0200

    docs: add comments/jsdocs to updateStockPrice util for clarity

--- stock-market-sim/src/app/utils/priceUtils.js
+++ stock-market-sim/src/app/utils/priceUtils.js
@@ -1,7 +1,12 @@
+/**
+ * Updates the stock price based on market volatility.
+ * @param {*} price - The current stock price.
+ * @returns The updated stock price with volatility applied.
+ */
 export function updateStockPrice(price) {
   const stockPrice = price;
   // Simulate volatility => 5% swing will be rare, 1% swings will be more common
-  const volatility = Math.random() < 0.1 ? 0.05 : 0.01;
+  const volatility = Math.random() < 0.1 ? 0.05 : 0.01; //(volatility could be made more variable later, i.e each company,sector, etc. could have different volatility levels)
   const change = (Math.random() * volatility * 2 - volatility) * stockPrice;
   const newPrice = parseFloat((stockPrice + change).toFixed(2));
   return newPrice;
